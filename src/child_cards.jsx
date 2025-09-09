fc7d307100223a95ac24fde3294a081e6dab4fc3
Your Name <you@example.com>
2025-09-09 10:41:21 +0530

    Seperate Child Card into Components

---
+++ src/child_cards.jsx
@@ -0,0 +1,115 @@
+export function CardOne() {
+  return (
+    <>
+      <div id="card-1" className="border-2 border-white flex-1 flex flex-col items-center ">
+        <img id="card-1-img" className="flex-5"></img>
+        <h2 id="card-1-title" className="font-sans text-4xl flex-1 ">
+          {'SYRAX'}
+        </h2>
+      </div>
+    </>
+  );
+}
+
+export function CardTwo() {
+    return (<>
+    
+            <div id="card-2" className="border-2 border-white flex-1 flex flex-col items-center ">
+          <img id="card-2-img" className="flex-5"></img>
+          <h2 id="card-2-title" className="font-sans text-4xl flex-1 ">
+            {'SYRAX'}
+          </h2>
+        </div>
+
+    
+    
+    
+    </>)
+}
+
+export function CardThree() {
+    return (
+        <>
+                <div id="card-3" className="border-2 border-white flex-1 flex flex-col items-center ">
+          <img id="card-3-img" className="flex-5"></img>
+          <h2 id="card-3-title" className="font-sans text-4xl flex-1 ">
+            {'SYRAX'}
+          </h2>
+        </div>        
+        </>
+    )
+}
+
+export function CardFour() {
+
+    return (
+<>
+                <div id="card-4" className="border-2 border-white flex-1 flex flex-col items-center ">
+          <img id="card-4-img" className="flex-5"></img>
+          <h2 id="card-4-title" className="font-sans text-4xl flex-1 ">
+            {'SYRAX'}
+          </h2>
+        </div>
+
+
+</>
+
+
+    )
+}
+export function CardFive() {
+    return (
+        <>
+                <div id="card-5" className="border-2 border-white flex-1 flex flex-col items-center ">
+          <img id="card-5-img" className="flex-5"></img>
+          <h2 id="card-5-title" className="font-sans text-4xl flex-1 ">
+            {'SYRAX'}
+          </h2>
+        </div>
+
+        </>
+    )
+}
+export function CardSix() {
+    return (
+
+        <>
+                <div id="card-6" className="border-2 border-white flex-1 flex flex-col items-center ">
+          <img id="card-6-img" className="flex-5"></img>
+          <h2 id="card-6-title" className="font-sans text-4xl flex-1 ">
+            {'SYRAX'}
+          </h2>
+        </div>
+
+        </>
+    )
+}
+export function CardSeven() {
+return (
+
+    <>
+            <div id="card-7" className="border-2 border-white flex-1 flex flex-col items-center ">
+          <img id="card-7-img" className="flex-5"></img>
+          <h2 id="card-7-title" className="font-sans text-4xl flex-1 ">
+            {'SYRAX'}
+          </h2>
+        </div>
+
+    </>
+)
+
+}
+export function CardEight() {
+    return (
+        <>
+                <div id="card-8" className="border-2 border-white flex-1 flex flex-col items-center ">
+          <img id="card-8-img" className="flex-5"></img>
+          <h2 id="card-8-title" className="font-sans text-4xl flex-1 ">
+            {'SYRAX'}
+          </h2>
+        </div>
+
+        
+        </>
+    )
+}
