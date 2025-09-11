19cfcaa5f2e78b52e5edf3b18062579594a3562d
Your Name <you@example.com>
2025-09-11 15:23:52 +0530

    Update styling

--- src/Mainpage.jsx
+++ src/Mainpage.jsx
@@ -4,15 +4,11 @@ import Cards from './cards';
 function MainPage() {
   const [scoreCounter, setScoreCounter] = useState(0);
 
-
-
-
-
-
-
   return (
     <div className="h-screen w-screen bg-[url(./assets/wallpaper_minecraft_nether_update_1920x1080.png)] bg-cover bg-center bg-no-repeat grid grid-rows-10 grid-cols-10 relative font-sans">
-      <h1 id='Game-title' className='row-start-1 col-start-1 col-end-4 text-xl font-sans'>Memorize The Mob </h1>
+      <h1 id="Game-title" className="row-start-1 col-start-1 col-end-4 text-xl font-sans">
+        Memorize The Mob{' '}
+      </h1>
       <Score scoreCounter={scoreCounter} />
       <h1 className=" font-sans text-2xl text-gray-50 row-start-2 col-start-3 col-end-11 " id="Result">
         {'Round Over Best Score is  :' + { scoreCounter }}
