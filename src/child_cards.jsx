6b3ff5f6512f7c14b56bf96b13682fa6827ce641
Your Name <you@example.com>
2025-09-11 15:26:19 +0530

    Complete and check all cards API Fetch and updating state

--- src/child_cards.jsx
+++ src/child_cards.jsx
@@ -1,242 +1,250 @@
 import { useEffect } from 'react';
 
-export function CardOne({ srcUrlArr, setSrcUrlArr }) {
-  (useEffect(() => {
+export function CardOne({ srcUrlArr, setSrcUrlArr, srcTitleArr, setSrcTitleArr }) {
+  useEffect(() => {
     const fetchImages = async () => {
       try {
         const promisedResponse = await fetch(
           'https://tenor.googleapis.com/v2/search?q=minecraftstrider&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=2',
         );
         const promisedResult = await promisedResponse.json();
-        console.log(promisedResult);
-return promisedResult;
-        //  setpromisedResults(promisedResults.push(promisedResult))
-        //  console.log(promisedResults);
+      
+
+        const imgSrc1 = promisedResult.results[0].media_formats.mediumgif.url;
+        const imgSrcTitle1 = promisedResult.results[0].tags[1].toUpperCase();
+        setSrcUrlArr((srcUrlArr) => [...srcUrlArr, imgSrc1]);
+        setSrcTitleArr((srcTitleArr) => [...srcTitleArr, imgSrcTitle1]);
+      
       } catch (error) {
-        console.log('Error in promisedResponse : ' + error);
+        console.log('Error in promisedResponse: ' + error);
       }
     };
+
     fetchImages();
-  }),
-    []);
+  }, []);
 
   return (
     <>
       <div id="card-1" className="border-2 border-white flex-1 flex flex-col items-center ">
-        <img id="card-1-img" className="flex-5"></img>
+        <img id="card-1-img" className="flex-5 bg-cover " src={srcUrlArr[0]}></img>
         <h2 id="card-1-title" className="font-sans text-4xl flex-1 ">
-          {'SYRAX'}
+          {srcTitleArr[0]}
         </h2>
       </div>
     </>
   );
 }
 
-export function CardTwo({ srcUrlArr, setSrcUrlArr }) {
-  (useEffect(() => {
+export function CardTwo({ srcUrlArr, setSrcUrlArr, srcTitleArr, setSrcTitleArr }) {
+  useEffect(() => {
     const fetchImages = async () => {
       try {
         const promisedResponse = await fetch(
           'https://tenor.googleapis.com/v2/search?q=minecraftblaze&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=4',
         );
         const promisedResult = await promisedResponse.json();
-return promisedResult;
-        //  setpromisedResults(promisedResults.push(promisedResult))
-        //  console.log(promisedResults);
+
+        const imgSrc2 = promisedResult.results[0].media_formats.mediumgif.url;
+        const imgSrcTitle2 = promisedResult.results[0].tags[0].toUpperCase();
+        setSrcUrlArr((srcUrlArr) => [...srcUrlArr, imgSrc2]);
+        setSrcTitleArr((srcTitleArr) => [...srcTitleArr, imgSrcTitle2]);
+      
       } catch (error) {
         console.log('Error in promisedResponse : ' + error);
       }
     };
     fetchImages();
-  }),
-    []);
+  }, []);
+
   return (
     <>
-      <div id="card-2" className="border-2 border-white flex-1 flex flex-col items-center ">
-        <img id="card-2-img" className="flex-5"></img>
+      <div id="card-2" className="border-2 border-white flex-1 flex flex-col items-center  ">
+        <img id="card-2-img" className="flex-5 bg-cover bg-center bg-no-repeat" src={srcUrlArr[1]}></img>
         <h2 id="card-2-title" className="font-sans text-4xl flex-1 ">
-          {'SYRAX'}
+          {srcTitleArr[1]}
         </h2>
       </div>
     </>
   );
 }
 
-export function CardThree({ srcUrlArr, setSrcUrlArr }) {
-  (useEffect(() => {
+export function CardThree({ srcUrlArr, setSrcUrlArr, srcTitleArr, setSrcTitleArr }) {
+  useEffect(() => {
     const fetchImages = async () => {
       try {
         const promisedResponse = await fetch(
-          'https://tenor.googleapis.com/v2/search?q=minecrafthoglin&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=4',
+          'https://tenor.googleapis.com/v2/search?q=hoglin&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=4',
         );
         const promisedResult = await promisedResponse.json();
-return promisedResult;
-        //  setpromisedResults(promisedResults.push(promisedResult))
-        //  console.log(promisedResults);
+        const imgSrc3 = promisedResult.results[1].media_formats.mediumgif.url;
+        const imgSrcTitle3 = promisedResult.results[1].tags[1].toUpperCase();
+        setSrcUrlArr((srcUrlArr) => [...srcUrlArr, imgSrc3]);
+        setSrcTitleArr((srcTitleArr) => [...srcTitleArr, imgSrcTitle3]);
+
       } catch (error) {
         console.log('Error in promisedResponse : ' + error);
       }
     };
     fetchImages();
-  }),
-    []);
+  }, []);
   return (
     <>
       <div id="card-3" className="border-2 border-white flex-1 flex flex-col items-center ">
-        <img id="card-3-img" className="flex-5"></img>
+        <img id="card-3-img" className="flex-5" src={srcUrlArr[2]}></img>
         <h2 id="card-3-title" className="font-sans text-4xl flex-1 ">
-          {'SYRAX'}
+          {srcTitleArr[2]}
         </h2>
       </div>
     </>
   );
 }
 
-export function CardFour({ srcUrlArr, setSrcUrlArr }) {
-  (useEffect(() => {
+export function CardFour({ srcUrlArr, setSrcUrlArr, srcTitleArr, setSrcTitleArr }) {
+  useEffect(() => {
     const fetchImages = async () => {
       try {
         const promisedResponse = await fetch(
-          'https://tenor.googleapis.com/v2/search?q=minecraftghast&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=4',
+          'https://tenor.googleapis.com/v2/search?q=ghast&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=4',
         );
         const promisedResult = await promisedResponse.json();
-        return promisedResult;
+        const imgSrc4 = promisedResult.results[0].media_formats.mediumgif.url;
+        const imgSrcTitle4 = promisedResult.results[0].tags[1].toUpperCase();
+        setSrcUrlArr((srcUrlArr) => [...srcUrlArr, imgSrc4]);
+        setSrcTitleArr((srcTitleArr) => [...srcTitleArr, imgSrcTitle4]);
 
-        //  setpromisedResults(promisedResults.push(promisedResult))
-        //  console.log(promisedResults);
       } catch (error) {
         console.log('Error in promisedResponse : ' + error);
       }
     };
     fetchImages();
-  }),
-    []);
+  }, []);
+
   return (
     <>
       <div id="card-4" className="border-2 border-white flex-1 flex flex-col items-center ">
-        <img id="card-4-img" className="flex-5"></img>
+        <img id="card-4-img" className="flex-5" src={srcUrlArr[3]}></img>
         <h2 id="card-4-title" className="font-sans text-4xl flex-1 ">
-          {'SYRAX'}
+          {srcTitleArr[3]}
         </h2>
       </div>
     </>
   );
 }
-export function CardFive({ srcUrlArr, setSrcUrlArr }) {
-  (useEffect(() => {
+export function CardFive({ srcUrlArr, setSrcUrlArr, srcTitleArr, setSrcTitleArr }) {
+  useEffect(() => {
     const fetchImages = async () => {
       try {
         const promisedResponse = await fetch(
-          'https://tenor.googleapis.com/v2/search?q=minecraftzombiepiglin&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=4',
+          'https://tenor.googleapis.com/v2/search?q=witherstorm&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=4',
         );
         const promisedResult = await promisedResponse.json();
-return promisedResult;
-
-        //  setpromisedResults(promisedResults.push(promisedResult))
-        //  console.log(promisedResults);
+        const imgSrc5 = promisedResult.results[2].media_formats.mediumgif.url;
+        const imgSrcTitle5 = promisedResult.results[2].tags[0].toUpperCase();
+        setSrcUrlArr((srcUrlArr) => [...srcUrlArr, imgSrc5]);
+        setSrcTitleArr((srcTitleArr) => [...srcTitleArr, imgSrcTitle5]);
       } catch (error) {
         console.log('Error in promisedResponse : ' + error);
       }
     };
     fetchImages();
-  }),
-    []);
+  }, []);
   return (
     <>
       <div id="card-5" className="border-2 border-white flex-1 flex flex-col items-center ">
-        <img id="card-5-img" className="flex-5"></img>
+        <img id="card-5-img" className="flex-5" src={srcUrlArr[4]}></img>
         <h2 id="card-5-title" className="font-sans text-4xl flex-1 ">
-          {'SYRAX'}
+          {srcTitleArr[4]}
         </h2>
       </div>
     </>
   );
 }
-export function CardSix({ srcUrlArr, setSrcUrlArr }) {
-  (useEffect(() => {
+export function CardSix({ srcUrlArr, setSrcUrlArr, srcTitleArr, setSrcTitleArr }) {
+useEffect(() => {
     const fetchImages = async () => {
       try {
         const promisedResponse = await fetch(
-          'https://tenor.googleapis.com/v2/search?q=minecraftwitherskeleton&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=4',
+          'https://tenor.googleapis.com/v2/search?q=minecraftwarden&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=4',
         );
         const promisedResult = await promisedResponse.json();
-        return promisedResult;
-
-        //  setpromisedResults(promisedResults.push(promisedResult))
-        //  console.log(promisedResults);
+        const imgSrc6 = promisedResult.results[1].media_formats.mediumgif.url;
+        const imgSrcTitle6 = promisedResult.results[0].tags[0].toUpperCase();
+        setSrcUrlArr((srcUrlArr) => [...srcUrlArr, imgSrc6]);
+        setSrcTitleArr((srcTitleArr) => [...srcTitleArr, imgSrcTitle6]);
       } catch (error) {
         console.log('Error in promisedResponse : ' + error);
       }
     };
     fetchImages();
-  }),
+  },
     []);
   return (
     <>
       <div id="card-6" className="border-2 border-white flex-1 flex flex-col items-center ">
-        <img id="card-6-img" className="flex-5"></img>
+        <img id="card-6-img" className="flex-5" src={srcUrlArr[5]}></img>
         <h2 id="card-6-title" className="font-sans text-4xl flex-1 ">
-          {'SYRAX'}
+          {srcTitleArr[5]}
         </h2>
       </div>
     </>
   );
 }
-export function CardSeven({ srcUrlArr, setSrcUrlArr }) {
-  (useEffect(() => {
+export function CardSeven({ srcUrlArr, setSrcUrlArr, srcTitleArr, setSrcTitleArr }) {
+  useEffect(() => {
     const fetchImages = async () => {
       try {
         const promisedResponse = await fetch(
           'https://tenor.googleapis.com/v2/search?q=minecraftenderman&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=4',
         );
         const promisedResult = await promisedResponse.json();
-return promisedResult;
+         const imgSrc7 = promisedResult.results[0].media_formats.mediumgif.url;
+        const imgSrcTitle7 = promisedResult.results[0].tags[0].toUpperCase();
+        setSrcUrlArr((srcUrlArr) => [...srcUrlArr, imgSrc7]);
+        setSrcTitleArr((srcTitleArr) => [...srcTitleArr, imgSrcTitle7]);
 
-        //  setpromisedResults(promisedResults.push(promisedResult))
-        //  console.log(promisedResults);
-      } catch (error) {
+           } catch (error) {
         console.log('Error in promisedResponse : ' + error);
       }
     };
     fetchImages();
-  }),
+  },
     []);
   return (
     <>
       <div id="card-7" className="border-2 border-white flex-1 flex flex-col items-center ">
-        <img id="card-7-img" className="flex-5"></img>
+        <img id="card-7-img" className="flex-5" src={srcUrlArr[6]}></img>
         <h2 id="card-7-title" className="font-sans text-4xl flex-1 ">
-          {'SYRAX'}
+          {srcTitleArr[6]}
         </h2>
       </div>
     </>
   );
 }
-export function CardEight({ srcUrlArr, setSrcUrlArr }) {
-  (useEffect(() => {
+export function CardEight({ srcUrlArr, setSrcUrlArr, srcTitleArr, setSrcTitleArr }) {
+  useEffect(() => {
     const fetchImages = async () => {
       try {
         const promisedResponse = await fetch(
-          'https://tenor.googleapis.com/v2/search?q=minecraftskeleton&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=4',
+          'https://tenor.googleapis.com/v2/search?q=minecraftcreeper&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=4',
         );
         const promisedResult = await promisedResponse.json();
-        return promisedResult;
-
-        //  setpromisedResults(promisedResults.push(promisedResult))
-        //  console.log(promisedResults);
+         const imgSrc8 = promisedResult.results[0].media_formats.mediumgif.url;
+        const imgSrcTitle8 = promisedResult.results[0].tags[0].toUpperCase();
+        setSrcUrlArr((srcUrlArr) => [...srcUrlArr, imgSrc8]);
+        setSrcTitleArr((srcTitleArr) => [...srcTitleArr, imgSrcTitle8]);
       } catch (error) {
         console.log('Error in promisedResponse : ' + error);
       }
     };
     fetchImages();
-  }),
+  },
     []);
   return (
     <>
       <div id="card-8" className="border-2 border-white flex-1 flex flex-col items-center ">
-        <img id="card-8-img" className="flex-5"></img>
+        <img id="card-8-img" className="flex-5" src={srcUrlArr[7]}></img>
         <h2 id="card-8-title" className="font-sans text-4xl flex-1 ">
-          {'SYRAX'}
+          {srcTitleArr[7]}
         </h2>
       </div>
     </>
