0b1e104c4ba92f0d2aefdf61e8d3b24170a50faa
Your Name <you@example.com>
2025-09-11 15:25:08 +0530

    Fix card4 Comp argument missing issue

--- src/cards.jsx
+++ src/cards.jsx
@@ -4,24 +4,63 @@ import { CardOne, CardTwo, CardThree, CardFour, CardFive, CardSix, CardSeven, Ca
 
 export default function Cards() {
   const [srcUrlArr, setSrcUrlArr] = useState([]);
-
+  const [srcTitleArr, setSrcTitleArr] = useState([]);
   return (
     <div
       id="card-parent-container"
-      className=" row-start-3 row-end-10 col-start-2 col-end-10 flex flex-col 
-gap-4"
+      className=" row-start-3 row-end-10 col-start-2 col-end-10 flex flex-col gap-4 h-[80vh] w-[90vw]"
     >
       <div id="card-container1" className=" flex flex-row flex-1 gap-4">
-        <CardOne srcUrlArr={srcUrlArr} setSrcUrlArr={setSrcUrlArr} />
-        <CardTwo srcUrlArr={srcUrlArr} setSrcUrlArr={setSrcUrlArr} />
-        <CardThree srcUrlArr={srcUrlArr} setSrcUrlArr={setSrcUrlArr} />
-        <CardFour srcUrlArr={srcUrlArr} setSrcUrlArr={setSrcUrlArr} />
+        <CardOne
+          srcUrlArr={srcUrlArr}
+          setSrcUrlArr={setSrcUrlArr}
+          srcTitleArr={srcTitleArr}
+          setSrcTitleArr={setSrcTitleArr}
+        />
+        <CardTwo
+          srcUrlArr={srcUrlArr}
+          setSrcUrlArr={setSrcUrlArr}
+          srcTitleArr={srcTitleArr}
+          setSrcTitleArr={setSrcTitleArr}
+        />
+        <CardThree
+          srcUrlArr={srcUrlArr}
+          setSrcUrlArr={setSrcUrlArr}
+          srcTitleArr={srcTitleArr}
+          setSrcTitleArr={setSrcTitleArr}
+        />
+        <CardFour
+          srcUrlArr={srcUrlArr}
+          setSrcUrlArr={setSrcUrlArr}
+          srcTitleArr={srcTitleArr}
+          setSrcTitleArr={setSrcTitleArr}
+        />
       </div>
       <div id="card-container2" className=" flex flex-row flex-1 gap-4">
-        <CardFive srcUrlArr={srcUrlArr} setSrcUrlArr={setSrcUrlArr} />
-        <CardSix srcUrlArr={srcUrlArr} setSrcUrlArr={setSrcUrlArr} />
-        <CardSeven srcUrlArr={srcUrlArr} setSrcUrlArr={setSrcUrlArr} />
-        <CardEight srcUrlArr={srcUrlArr} setSrcUrlArr={setSrcUrlArr} />
+        <CardFive
+          srcUrlArr={srcUrlArr}
+          setSrcUrlArr={setSrcUrlArr}
+          srcTitleArr={srcTitleArr}
+          setSrcTitleArr={setSrcTitleArr}
+        />
+        <CardSix
+          srcUrlArr={srcUrlArr}
+          setSrcUrlArr={setSrcUrlArr}
+          srcTitleArr={srcTitleArr}
+          setSrcTitleArr={setSrcTitleArr}
+        />
+        <CardSeven
+          srcUrlArr={srcUrlArr}
+          setSrcUrlArr={setSrcUrlArr}
+          srcTitleArr={srcTitleArr}
+          setSrcTitleArr={setSrcTitleArr}
+        />
+        <CardEight
+          srcUrlArr={srcUrlArr}
+          setSrcUrlArr={setSrcUrlArr}
+          srcTitleArr={srcTitleArr}
+          setSrcTitleArr={setSrcTitleArr}
+        />
       </div>
     </div>
   );
