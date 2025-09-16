7fc9c2cc02b374d7ffdd21ebe572845148825b49
Your Name <you@example.com>
2025-09-16 10:36:17 +0530

    Accumulate all fetch calls and responses

--- src/cards.jsx
+++ src/cards.jsx
@@ -1,34 +1,60 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { CardOne, CardTwo, CardThree, CardFour, CardFive, CardSix, CardSeven, CardEight } from './child_cards';
 
 export default function Cards() {
   const [srcUrlArr, setSrcUrlArr] = useState([]);
-  const [srcTitleArr, setSrcTitleArr] = useState([]);
+  useEffect(() => console.log(srcUrlArr));
+  const urlArr = [
+    'https://tenor.googleapis.com/v2/search?q=minecraftstrider&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=2',
+    'https://tenor.googleapis.com/v2/search?q=minecraftblaze&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=4',
+    'https://tenor.googleapis.com/v2/search?q=hoglin&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=4',
+    'https://tenor.googleapis.com/v2/search?q=ghast&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=4',
+    'https://tenor.googleapis.com/v2/search?q=witherstorm&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=4',
+    'https://tenor.googleapis.com/v2/search?q=minecraftwarden&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=4',
+    'https://tenor.googleapis.com/v2/search?q=minecraftenderman&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=4',
+    'https://tenor.googleapis.com/v2/search?q=minecraftcreeper&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=4',
+  ];
+  useEffect(() => {
+    
+    urlArr.forEach((element) => {
+      const fetchData = async () => {
+        try {
+          const promisedResponse = await fetch(element);
+          const promisedResult = await promisedResponse.json();
+          console.log(promisedResult);
 
-  const handleFetchedImg = (newResponse) => {
-    setSrcUrlArr((srcUrlArr) => {
-      if (srcUrlArr.length === 0) {
-        return [...srcUrlArr, newResponse];
-      }
-      if (srcUrlArr.find((element) => element === newResponse)) {
-        return srcUrlArr;
-      }
-      return [...srcUrlArr, newResponse];
+          let imgTitleSrc = {
+            img: promisedResult.results[0].media_formats.mediumgif.url,
+            title: promisedResult.results[0].tags[0],
+          };
+
+          setSrcUrlArr((prevArr) => {
+            if (prevArr.some((e) => e.img === imgTitleSrc.img)) return prevArr;
+            return [...prevArr, imgTitleSrc];
+          });
+        } catch (error) {
+          console.log('Error:', error);
+        }
+      };
+      fetchData();
     });
-  };
+  }, []);
+  
+  const giveRandomNumArr = () => {
+    const randomNumArr = [];
+    randomNumArr.map(() => {
+      const randomNum = Math.floor(Math.random() * 8);
 
-  const handleFetchedTitle = (newResponse) => {
-    setSrcTitleArr((srcTitleArr) => {
-      if (srcTitleArr.length === 0) {
-        return [...srcTitleArr, newResponse];
-      }
-      if (srcTitleArr.find((element) => element === newResponse)) {
-        return srcTitleArr;
-      }
-
-      return [...srcTitleArr, newResponse];
+      randomNumArr.length === 0 ? randomNumArr.push(randomNum) : null;
+      randomNumArr.find((element) => {
+        element !== randomNum;
+      })
+        ? randomNumArr.push(randomNum)
+        : null;
     });
+
+    return randomNumArr;
   };
 
   return (
@@ -37,56 +63,16 @@ export default function Cards() {
       className=" row-start-3 row-end-10 col-start-2 col-end-10 flex flex-col gap-4 h-[80vh] w-[90vw]"
     >
       <div id="card-container1" className=" flex flex-row flex-1 gap-4">
-        <CardOne
-          srcUrlArr={srcUrlArr}
-          handleFetchedImg={handleFetchedImg}
-          srcTitleArr={srcTitleArr}
-          handleFetchedTitle={handleFetchedTitle}
-        />
-        <CardTwo
-          srcUrlArr={srcUrlArr}
-          handleFetchedImg={handleFetchedImg}
-          srcTitleArr={srcTitleArr}
-          handleFetchedTitle={handleFetchedTitle}
-        />
-        <CardThree
-          srcUrlArr={srcUrlArr}
-          handleFetchedImg={handleFetchedImg}
-          srcTitleArr={srcTitleArr}
-          handleFetchedTitle={handleFetchedTitle}
-        />
-        <CardFour
-          srcUrlArr={srcUrlArr}
-          handleFetchedImg={handleFetchedImg}
-          srcTitleArr={srcTitleArr}
-          handleFetchedTitle={handleFetchedTitle}
-        />
+        <CardOne srcUrlArr={srcUrlArr} />
+        <CardTwo srcUrlArr={srcUrlArr} />
+        <CardThree srcUrlArr={srcUrlArr} />
+        <CardFour srcUrlArr={srcUrlArr} />
       </div>
       <div id="card-container2" className=" flex flex-row flex-1 gap-4">
-        <CardFive
-          srcUrlArr={srcUrlArr}
-          handleFetchedImg={handleFetchedImg}
-          srcTitleArr={srcTitleArr}
-          handleFetchedTitle={handleFetchedTitle}
-        />
-        <CardSix
-          srcUrlArr={srcUrlArr}
-          handleFetchedImg={handleFetchedImg}
-          srcTitleArr={srcTitleArr}
-          handleFetchedTitle={handleFetchedTitle}
-        />
-        <CardSeven
-          srcUrlArr={srcUrlArr}
-          handleFetchedImg={handleFetchedImg}
-          srcTitleArr={srcTitleArr}
-          handleFetchedTitle={handleFetchedTitle}
-        />
-        <CardEight
-          srcUrlArr={srcUrlArr}
-          handleFetchedImg={handleFetchedImg}
-          srcTitleArr={srcTitleArr}
-          handleFetchedTitle={handleFetchedTitle}
-        />
+        <CardFive srcUrlArr={srcUrlArr} />
+        <CardSix srcUrlArr={srcUrlArr} />
+        <CardSeven srcUrlArr={srcUrlArr} />
+        <CardEight srcUrlArr={srcUrlArr} />
       </div>
     </div>
   );
