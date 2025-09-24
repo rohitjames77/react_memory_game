5960109c4416cf74e0003fdfe7f415bf0f00f1ec
Your Name <you@example.com>
2025-09-24 14:30:45 +0530

    Add Shuffle State Array Logic

--- src/cards.jsx
+++ src/cards.jsx
@@ -1,28 +1,26 @@
 import { useEffect, useState } from 'react';
-
-import { CardOne, CardTwo, CardThree, CardFour, CardFive, CardSix, CardSeven, CardEight } from './child_cards';
+import Chance from 'chance';
+import { Child_Card } from './child_cards';
 
 export default function Cards() {
   const [srcUrlArr, setSrcUrlArr] = useState([]);
-  useEffect(() => console.log(srcUrlArr));
+
   const urlArr = [
     'https://tenor.googleapis.com/v2/search?q=minecraftstrider&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=2',
-    'https://tenor.googleapis.com/v2/search?q=minecraftblaze&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=4',
-    'https://tenor.googleapis.com/v2/search?q=hoglin&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=4',
-    'https://tenor.googleapis.com/v2/search?q=ghast&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=4',
-    'https://tenor.googleapis.com/v2/search?q=witherstorm&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=4',
-    'https://tenor.googleapis.com/v2/search?q=minecraftwarden&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=4',
-    'https://tenor.googleapis.com/v2/search?q=minecraftenderman&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=4',
-    'https://tenor.googleapis.com/v2/search?q=minecraftcreeper&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=4',
+    'https://tenor.googleapis.com/v2/search?q=minecraftblaze&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=2',
+    'https://tenor.googleapis.com/v2/search?q=hoglin&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=2',
+    'https://tenor.googleapis.com/v2/search?q=ghast&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=2',
+    'https://tenor.googleapis.com/v2/search?q=witherstorm&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=2',
+    'https://tenor.googleapis.com/v2/search?q=minecraftwarden&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=2',
+    'https://tenor.googleapis.com/v2/search?q=minecraftenderman&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=2',
+    'https://tenor.googleapis.com/v2/search?q=minecraftcreeper&key=AIzaSyDzHHxZeWLb7pVTml_arg_-qAvqqba7XL4&limit=2',
   ];
   useEffect(() => {
-    
     urlArr.forEach((element) => {
       const fetchData = async () => {
         try {
           const promisedResponse = await fetch(element);
           const promisedResult = await promisedResponse.json();
-          console.log(promisedResult);
 
           let imgTitleSrc = {
             img: promisedResult.results[0].media_formats.mediumgif.url,
@@ -40,22 +38,19 @@ export default function Cards() {
       fetchData();
     });
   }, []);
-  
-  const giveRandomNumArr = () => {
-    const randomNumArr = [];
-    randomNumArr.map(() => {
-      const randomNum = Math.floor(Math.random() * 8);
 
-      randomNumArr.length === 0 ? randomNumArr.push(randomNum) : null;
-      randomNumArr.find((element) => {
-        element !== randomNum;
-      })
-        ? randomNumArr.push(randomNum)
-        : null;
-    });
+  let createShuffledSrcArr = (array) => {
+    let newArray = [...array];
+    for (let i = newArray.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+    }
 
-    return randomNumArr;
+    return newArray;
   };
+  useEffect(() => {
+    createShuffledSrcArr([1, 2, 3, 4, 5, 6]);
+  }, []);
 
   return (
     <div
@@ -63,16 +58,64 @@ export default function Cards() {
       className=" row-start-3 row-end-10 col-start-2 col-end-10 flex flex-col gap-4 h-[80vh] w-[90vw]"
     >
       <div id="card-container1" className=" flex flex-row flex-1 gap-4">
-        <CardOne srcUrlArr={srcUrlArr} />
-        <CardTwo srcUrlArr={srcUrlArr} />
-        <CardThree srcUrlArr={srcUrlArr} />
-        <CardFour srcUrlArr={srcUrlArr} />
+        <Child_Card
+          srcUrlArr={srcUrlArr}
+          createShuffledSrcArr={createShuffledSrcArr}
+          setSrcUrlArr={setSrcUrlArr}
+          srcObjImg={srcUrlArr[0]?.img}
+          srcObjTitle={srcUrlArr[0]?.title}
+        />
+        <Child_Card
+          srcUrlArr={srcUrlArr}
+          createShuffledSrcArr={createShuffledSrcArr}
+          setSrcUrlArr={setSrcUrlArr}
+          srcObjImg={srcUrlArr[1]?.img}
+          srcObjTitle={srcUrlArr[1]?.title}
+        />
+        <Child_Card
+          srcUrlArr={srcUrlArr}
+          createShuffledSrcArr={createShuffledSrcArr}
+          setSrcUrlArr={setSrcUrlArr}
+          srcObjImg={srcUrlArr[2]?.img}
+          srcObjTitle={srcUrlArr[2]?.title}
+        />
+        <Child_Card
+          srcUrlArr={srcUrlArr}
+          createShuffledSrcArr={createShuffledSrcArr}
+          setSrcUrlArr={setSrcUrlArr}
+          srcObjImg={srcUrlArr[3]?.img}
+          srcObjTitle={srcUrlArr[3]?.title}
+        />
       </div>
       <div id="card-container2" className=" flex flex-row flex-1 gap-4">
-        <CardFive srcUrlArr={srcUrlArr} />
-        <CardSix srcUrlArr={srcUrlArr} />
-        <CardSeven srcUrlArr={srcUrlArr} />
-        <CardEight srcUrlArr={srcUrlArr} />
+        <Child_Card
+          srcUrlArr={srcUrlArr}
+          createShuffledSrcArr={createShuffledSrcArr}
+          setSrcUrlArr={setSrcUrlArr}
+          srcObjImg={srcUrlArr[4]?.img}
+          srcObjTitle={srcUrlArr[4]?.title}
+        />
+        <Child_Card
+          srcUrlArr={srcUrlArr}
+          createShuffledSrcArr={createShuffledSrcArr}
+          setSrcUrlArr={setSrcUrlArr}
+          srcObjImg={srcUrlArr[5]?.img}
+          srcObjTitle={srcUrlArr[5]?.title}
+        />
+        <Child_Card
+          srcUrlArr={srcUrlArr}
+          createShuffledSrcArr={createShuffledSrcArr}
+          setSrcUrlArr={setSrcUrlArr}
+          srcObjImg={srcUrlArr[6]?.img}
+          srcObjTitle={srcUrlArr[6]?.title}
+        />
+        <Child_Card
+          srcUrlArr={srcUrlArr}
+          createShuffledSrcArr={createShuffledSrcArr}
+          setSrcUrlArr={setSrcUrlArr}
+          srcObjImg={srcUrlArr[7]?.img}
+          srcObjTitle={srcUrlArr[7]?.title}
+        />
       </div>
     </div>
   );
